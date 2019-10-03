e86eb805e6e43624495970fd7ec298ee8c9b7d98
ahmed Waheed <aw0reborn@gmail.com>
2019-10-03 22:38:58 +0500

    done

--- 02-diagonal-difference/diagonal-difference.py
+++ 02-diagonal-difference/diagonal-difference.py
@@ -6,29 +6,17 @@ import random
 import re
 import sys
 
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
 
 def diagonalDifference(arr):
-    # Write your code here
+    l_to_r = 0
+    r_to_l = 0
+    for i in range(len(arr)):
+        l_to_r += arr[i][i]
+        r_to_l += arr[i][len(arr)-i-1]
+    result = abs(l_to_r-r_to_l)
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    return result
+arr = [[-11, 2, 4], [4, 5, 6], [10, 8, -12]]
+result = diagonalDifference(arr)
 
-    n = int(input().strip())
-
-    arr = []
-
-    for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    result = diagonalDifference(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
-
+print(result)
